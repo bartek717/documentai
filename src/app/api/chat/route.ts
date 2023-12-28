bf55390a29586c5fb51eadb876439e91f760b295
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-12-28 02:01:36 -0500

    chat works better. tmrw I make editor better. gn

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,36 +1,45 @@
-import { Configuration, OpenAIApi } from "openai-edge"
-import { OpenAIStream, StreamingTextResponse } from "ai"
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const runtime = 'edge';
 
+interface ChatMessage {
+  text: string;
+  sender: string; // 'user' or 'gpt4'
+}
+
+// Assuming the OpenAI API expects these roles
+type ChatCompletionRequestMessageRole = 'user' | 'assistant';
+
 const configuration = new Configuration({
-    apiKey: process.env.GPT_API_KEY
+  apiKey: process.env.GPT_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
 
-export async function POST(req: Request) {
-    if (req.method === 'POST') {
-        const { text } = await req.json();
+export async function POST(req: Request, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { messages } = await req.json() as { messages: ChatMessage[] };
+    try {
+      const formattedMessages = messages.map(msg => ({
+        role: msg.sender === 'user' ? 'user' : 'assistant' as ChatCompletionRequestMessageRole,
+        content: msg.text
+      }));
 
-        const response = await openai.createChatCompletion({
-            model: "gpt-4",
-            stream: true,
-            max_tokens: 4096, // No max tokens: super short / cut off response.
-            messages: [ // GPT-4 with Vision is JUST GPT-4. So you can still talk with it like GPT-4
-                // There is no "system" message (THIS MAY CHANGE)
-                {
-                    role: "user",
-                    //@ts-ignore
-                    content: text
-                    
-                }
-            ]
-        });
-        const stream = OpenAIStream(response);
+      const response = await openai.createChatCompletion({
+        model: "gpt-4",
+        stream: true,
+        max_tokens: 4096,
+        messages: formattedMessages
+      });
+      const stream = OpenAIStream(response);
 
-        return new StreamingTextResponse(stream);
+      return new StreamingTextResponse(stream);
     }
-
+    catch (error) {
+      console.error('Error in API call:', error);
+      res.status(500).json({ error: 'Error processing your request' });
+    }
   }
-  
+}
--- src/app/components/Chat.js
+++ src/app/components/Chat.js
@@ -1,39 +1,46 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './chatstyles.css';
 
 function Chat() {
   const [inputText, setInputText] = useState('');
   const [messages, setMessages] = useState([]);
-  const [ openAIResponse, setOpenAIResponse ] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add the user message to the messages array
-    setMessages(messages => [...messages, { text: inputText, sender: 'user' }]);
-
-    // Clear the input field immediately after submission
+    const newUserMessage = { text: inputText, sender: 'user' };
+    const updatedMessages = [...messages, newUserMessage];
+    setMessages(updatedMessages);
+  
     setInputText('');
-
-    // Make API call with inputText here
+  
     await fetch("api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ text: inputText })
+        body: JSON.stringify({ messages: updatedMessages })
       })
       .then(async (response) => {
         const reader = response.body?.getReader();
-        setOpenAIResponse("");
+        let currentChunk = '';
 
         while (true) {
           const { done, value } = await reader?.read();
-          if(done) break;
-          var currentChunk = new TextDecoder().decode(value);
-          
-          // Update the messages array with the new chunk
-          setOpenAIResponse((prev) => prev + currentChunk);
+          if (done) break;
+          currentChunk += new TextDecoder().decode(value);
+          setMessages(currentMessages => {
+            const newMessages = [...currentMessages];
+            if (newMessages.length > 0) {
+              const lastMessageIndex = newMessages.length - 1;
+              if (newMessages[lastMessageIndex].sender === 'user') {
+                newMessages.push({ text: currentChunk, sender: 'gpt4' });
+              } else {
+                newMessages[lastMessageIndex].text = currentChunk;
+              }
+            }
+            return newMessages;
+          });
         }
       });
   };
@@ -42,13 +49,10 @@ function Chat() {
     <div className='main-chat'>
       <div className="chat-messages">
         {messages.map((message, index) => (
-          <div key={index} className={message.sender}>
+          <div key={index} className={`message ${message.sender}`}>
             {message.text}
           </div>
         ))}
-        <div>
-            {openAIResponse}
-        </div>
       </div>
 
       <form onSubmit={handleSubmit}>
