e570cd66928a1cafd6ff118c6f20d80b1befe3b5
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-12-28 01:04:49 -0500

    my battle with height styling has concluded.... for now

---
+++ src/app/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './chatstyles.css'
+
+function Chat() {
+  return (
+    <div className='main-chat'>
+        chat
+    </div>
+  )
+}
+
+export default Chat
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import TipTap from './components/TipTap'
+import Image from 'next/image';
+import TipTap from './components/TipTap';
+import Chat from './components/Chat';
+import './styles.css';
 
 export default function Home() {
   return (
-    <main>
-      <TipTap></TipTap>
-    </main>
-  )
+    
+    <div className="main">
+      <div className="component" style={{ flex: '0 0 70%' }}>
+        <TipTap></TipTap>
+      </div>
+      <div className="component" style={{ flex: '0 0 30%' }}>
+        <Chat></Chat>
+      </div>
+    </div>
+    
+  );
 }
