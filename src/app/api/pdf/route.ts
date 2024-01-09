ad4d279786951ac6f554560f55e213caccd777ee
bartek717 <bartekkowalski925@gmail.com>
2024-01-09 17:17:34 -0500

    try edgeChromium Soulution

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,13 +1,15 @@
-import puppeteer from 'puppeteer';
+
 import type { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
-import { chromium } from 'playwright';
+import edgeChromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
 
 export async function POST(req: Request) {
   if (req.method === 'POST') {
     const { html, margins, font, fontSize } = await req.json();
+    const executablePath = await edgeChromium.executablePath
     const dynamicCss = `
     .editor-content {
       padding-top: ${margins.top};
@@ -22,10 +24,15 @@ export async function POST(req: Request) {
     `;
     const wrappedHtml = `<div class="editor-content">${html}</div>`;
     const fullHtml = `<style>${dynamicCss}</style>${wrappedHtml}`
-    const browser = await chromium.launch({headless: true});
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: edgeChromium.args,
+      headless: false,
+    })
+    
     const page = await browser.newPage();
-    await page.setContent(fullHtml, { waitUntil: 'networkidle' });
-    const pdfBuffer = await page.pdf({ format: 'A4' });
+    await page.setContent(fullHtml, { waitUntil: 'networkidle0' });
+    const pdfBuffer = await page.pdf({ format: 'a4' });
     await browser.close();
     const response = new NextResponse(pdfBuffer);
     response.headers.set('Content-Type', 'application/pdf');
